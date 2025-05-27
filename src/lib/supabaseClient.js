b6d3f8f9e280729ed93331270d3ffab4822f0423
sebastian073 <116037303+sebastian073@users.noreply.github.com>
2025-05-27 04:46:32 +0000

    cambios2

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-id.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://your-https://lppoyhkzsyoynirwcafo.supabase.co-id.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwcG95aGt6c3lveW5pcndjYWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMTU5MjksImV4cCI6MjA2Mzg5MTkyOX0.7wo-Wqq1tCZy0Cr-ToT3ZhuxwbnSHjJqMjGcbdTRMg0';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
